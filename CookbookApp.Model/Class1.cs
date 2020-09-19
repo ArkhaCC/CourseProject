91a69ce1917895516ed44846e644a75be8624fef
Cody Cates <ccates013@gmail.com>
2020-09-18 22:47:19 -0400

    Uploaded CookbookApp.Model/Class1 (rough design for database + initial migration. Revised PrelimDatabaseDesign.

---
+++ CookbookApp.Model/Class1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace CookbookApp.Model
+{
+    using Microsoft.EntityFrameworkCore;
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    namespace CookbookApp.Model
+    {
+        public class RecipeContext : DbContext
+        {
+           public DbSet<Recipe> Recipes { get; set; }
+           public DbSet<Tags> Posts { get; set; }
+
+            protected override void OnConfiguring(DbContextOptionsBuilder options)
+                => options.UseSqlite("Data Source=cookbook.db");
+        }
+
+        public class User
+        {
+            [Key]
+            public int UserName { get; set; }
+            public string LastLogin { get; set; }
+
+            public List<Recipe> Recipes { get; } = new List<Recipe>();
+        }
+
+        public class Recipe
+        {
+            [Key]
+            public string RecipeNameId { get; set; }
+            public byte[] SavedRecipe { get; set; }
+            public DateTime DateSaved { get; set; }
+
+            public Tags AppliedTags { get; set; }
+
+        }
+
+        public class Tags
+        {
+            [Key]
+            public string TagName { get; set; }
+            public Recipe recipe { get; }
+        }
+
+        public class AccessFrequency
+        {
+            [Key]
+            public Recipe recipe { get; }
+            public DateTime DateLastAccessed { get; set; }
+            public int TimesAccessed { get; set; }
+        }
+    }
+}
+
---
+++ CookbookApp.Model/Migrations/20200919024230_InitialCreate.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CookbookApp.Model.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Posts",
+                columns: table => new
+                {
+                    TagName = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Posts", x => x.TagName);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Recipes",
+                columns: table => new
+                {
+                    RecipeNameId = table.Column<string>(nullable: false),
+                    SavedRecipe = table.Column<byte[]>(nullable: true),
+                    DateSaved = table.Column<DateTime>(nullable: false),
+                    AppliedTagsTagName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Recipes", x => x.RecipeNameId);
+                    table.ForeignKey(
+                        name: "FK_Recipes_Posts_AppliedTagsTagName",
+                        column: x => x.AppliedTagsTagName,
+                        principalTable: "Posts",
+                        principalColumn: "TagName",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Recipes_AppliedTagsTagName",
+                table: "Recipes",
+                column: "AppliedTagsTagName");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Recipes");
+
+            migrationBuilder.DropTable(
+                name: "Posts");
+        }
+    }
+}
